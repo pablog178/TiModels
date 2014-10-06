8b2d1256deb3b483d2fea617e3b51c45207377d9
Pablo Guevara <pablog.178@gmail.com>
2014-10-06 15:36:26 -0500

    Redoing example app, it was more complex than what was really needed

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,45 +1,13 @@
-var App 		= require('core');
-var session 	= Alloy.Models.instance('session');
 var userModel 	= Alloy.Models.instance('user');
-var login;
-
-userModel.on('change:loggedIn', handleLoggedInChange);
 
 function init(){
+	userModel.fetch();
 	if(OS_IOS){
 		App.navWindow = $.navigationWindow;
 		$.navigationWindow.open();
 	} else {
-		$.mainScreen = Alloy.createController('mainScreen');
-		$.mainScreen.open();
-	}
-
-	session.fetch();
-	if(session.get('stayActive')){
-		userModel.login({
-			username : session.get('username'),
-			password : session.get('password'),
-			failure : openLogin
-		});
-	} else {
-		openLogin();
+		$.main.open();
 	}
 };
-function handleLoggedInChange () {
-	console.log('userModel: ' + JSON.stringify(userModel, null, '\t'));
-	if(userModel.get('loggedIn')){
-		closeLogin();
-	} else {
-		openLogin();
-	}
-}
-function openLogin(){
-	!login && (login = Alloy.createController('login'));
-	login.open();
-};
-function closeLogin () {
-	login && login.close();
-	login = null;
-}
 
 init();
--- app/models/user.js
+++ app/models/user.js
@@ -5,22 +5,17 @@ exports.definition = {
 	config: {
 		columns : {
 			'username' 	: 'TEXT UNIQUE',
-			'password' 	: 'TEXT',
 			'firstName' : 'TEXT',
 			'lastName' 	: 'TEXT',
 			'bornDate' 	: 'DATE',
-			'lastLogin' : 'DATE',
-			// Foreing Keys
-			'addressID' : 'TEXT'
+			'lastLogin' : 'DATE'
 		},
 		defaults : {
-			'username' 	: null,
-			'password'	: '',
+			'username' 	: 'default',
 			'firstName' : '',
 			'lastName' 	: '',
-			'lastLogin' : null,
-			'bornDate' 	: null,
-			'loggedIn' 	: false
+			'lastLogin' : new moment(),
+			'bornDate' 	: null
 		},
 		relations : {
 			'notes' : {
@@ -41,70 +36,7 @@ exports.definition = {
 	},
 	extendModel: function(Model) {
 		_.extend(Model.prototype, tiModel.modelBase, {
-			ERROR_UNKNOWN_ERROR : 0,
-			ERROR_NO_USER : 1,
-			ERROR_WRONG_LOGIN : 2,
-			login : function(params) {
-				params = params || {};
-				var _username = params.username;
-				var _password = params.password;
-				var _code = this.ERROR_UNKNOWN_ERROR;
-				
-				this.clear();
-				
-				if (_username && _password) {
-					this
-						.query()
-						.where('username = ?', _username)
-						.where('password = ?', _password)
-						.fetch();
-				}
-				
-				if (this.id) {
-					this.set({
-						lastLogin : new moment(),
-						loggedIn : true
-					});
-					params.success && params.success();
-				} else {
-					this
-						.clear()
-						.query()
-						.select('count(*) as count')
-						.where('username = ?', _username)
-						.fetch();
-
-					if(this.get('count') > 0){
-						_code = this.ERROR_WRONG_LOGIN;
-					} else {
-						_code = this.ERROR_NO_USER;
-					}
-
-					this.clear();
-
-					params.failure && params.failure({
-						code : _code
-					});
-				}
-			},
-			logout : function (params){
-				params = params || {};
-				this.clear();
-			},
-			createNew : function(params){
-				params = params || {};
-				var _username = params.username;
-				var _password = params.password;
-
-				if(_username && _password){
-					this.set({
-						username : _username,
-						password : _password,
-						lastLogin : new moment(),
-						loggedIn : true
-					}).save();
-				}
-			}
+			// extended functions and properties go here
 		});
 
 		return Model;
